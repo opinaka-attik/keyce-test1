21b64395ba35900e41588e6d4ec6807748bb6e54
opinaka <mohammed.attik@opinaka.com>
2026-05-12 14:56:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Docker + Vercel 🚀");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
